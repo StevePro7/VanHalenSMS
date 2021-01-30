bba7ac326c2d892920f0c6af57e0d013ba2d1281
StevePro7 <steveprozune@hotmail.com>
2021-01-30 16:38:07 +0000

    no message

--- dev/engine/content_manager.c
+++ dev/engine/content_manager.c
@@ -13,9 +13,9 @@ void engine_content_manager_load_tiles()
 	devkit_SMS_loadPSGaidencompressedTiles( font__tiles__psgcompr, FONT_TILES );
 	//devkit_SMS_loadBGPalette( ( void * ) font__palette__bin );
 
-	devkit_SMS_loadPSGaidencompressedTiles( VanHalenTitle__tiles__psgcompr, SPLASH_TILES );
-	devkit_SMS_loadSTMcompressedTileMap( 0, 0, ( void * ) VanHalenTitle__tilemap__stmcompr );
-	devkit_SMS_loadBGPalette( ( void * ) VanHalenTitle__palette__bin );
+	//devkit_SMS_loadPSGaidencompressedTiles( VanHalenTitle__tiles__psgcompr, SPLASH_TILES );
+	//devkit_SMS_loadSTMcompressedTileMap( 0, 0, ( void * ) VanHalenTitle__tilemap__stmcompr );
+	//devkit_SMS_loadBGPalette( ( void * ) VanHalenTitle__palette__bin );
 
 	// Set the font tile text to white.
 	devkit_SMS_setBGPaletteColor( 15, 3, 3, 3 );
@@ -26,6 +26,6 @@ void engine_content_manager_load_tiles()
 void engine_content_manager_load_sprites()
 {
 	// Sprite tiles.
-	devkit_SMS_loadPSGaidencompressedTiles( sprites__tiles__psgcompr, SPRITE_TILES );
-	devkit_SMS_loadSpritePalette( ( void * ) sprites__palette__bin );
+	devkit_SMS_loadPSGaidencompressedTiles( sprite__tiles__psgcompr, SPRITE_TILES );
+	devkit_SMS_loadSpritePalette( ( void * ) sprite__palette__bin );
 }
