ca0abf4283edadd390a17ed5506e3fbf525bf1f6
StevePro7 <steveprozune@hotmail.com>
2021-01-30 11:45:41 +0000

    no message

---
+++ dev/gfx.h
@@ -0,0 +1,15 @@
+extern const unsigned char	font__palette__bin[];
+#define				font__palette__bin_size 16
+
+extern const unsigned char	font__tilemap__bin[];
+#define				font__tilemap__bin_size 128
+
+extern const unsigned char	font__tiles__psgcompr[];
+#define				font__tiles__psgcompr_size 663
+
+extern const unsigned char	sprite__palette__bin[];
+#define				sprite__palette__bin_size 16
+
+extern const unsigned char	sprite__tiles__psgcompr[];
+#define				sprite__tiles__psgcompr_size 62
+
