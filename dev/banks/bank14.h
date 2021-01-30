bd515ac60b17418fe58b8d130089ec4bc0f9adf8
StevePro7 <steveprozune@hotmail.com>
2021-01-30 11:47:25 +0000

    no message

--- dev/banks/bank14.h
+++ dev/banks/bank14.h
@@ -2,3 +2,15 @@ extern const unsigned char	bank14_txt[];
 #define				bank14_txt_size 6
 #define				bank14_txt_bank 14
 
+extern const unsigned char	VanHalenTitle__palette__bin[];
+#define				VanHalenTitle__palette__bin_size 16
+#define				VanHalenTitle__palette__bin_bank 14
+
+extern const unsigned char	VanHalenTitle__tilemap__stmcompr[];
+#define				VanHalenTitle__tilemap__stmcompr_size 94
+#define				VanHalenTitle__tilemap__stmcompr_bank 14
+
+extern const unsigned char	VanHalenTitle__tiles__psgcompr[];
+#define				VanHalenTitle__tiles__psgcompr_size 4592
+#define				VanHalenTitle__tiles__psgcompr_bank 14
+
