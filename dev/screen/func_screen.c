dea8e759b7e38f51eab1e92ca29ccbfd46343467
StevePro7 <steveprozune@hotmail.com>
2021-02-01 13:17:41 +0000

    no message

--- dev/screen/func_screen.c
+++ dev/screen/func_screen.c
@@ -5,18 +5,36 @@
 #include "..\engine\font_manager.h"
 #include "..\engine\global_manager.h"
 #include "..\engine\input_manager.h"
+#include "..\engine\locale_manager.h"
 #include "..\engine\sprite_manager.h"
+#include "..\devkit\_sms_manager.h"
+
+static const unsigned char x = 4;
+static const unsigned char y = 20;
 
 void screen_func_screen_load()
 {
+	devkit_SMS_displayOff();
 	engine_content_manager_load_tiles();
+	engine_content_manager_load_title();
 	engine_content_manager_load_sprites();
-	engine_font_manager_draw_text( "FUNC SCREEN...!!", 2, 10 );
-	engine_font_manager_draw_text( "1984", 4, 4 );
+	engine_font_manager_draw_text( LOCALE_RECORD_TEXT, 6, 12 );
+	engine_font_manager_draw_text( LOCALE_COVERS_TEXT, 20, 12 );
+
+	engine_font_manager_draw_text( "1984", x, y );
+	engine_font_manager_draw_text( "1986", x+7, y );
+	engine_font_manager_draw_text( "1988", x+14, y );
+	devkit_SMS_displayOn();
+
+	
+	//engine_font_manager_draw_text( "FUNC SCREEN...!!", 2, 10 );
+	//engine_font_manager_draw_text( "1984", 4, 4 );
 }
 
 void screen_func_screen_update( unsigned char *screen_type )
 {
+	unsigned const char sx = ( x - 1 ) * 8;
+	unsigned const char sy = ( y - 1 ) * 8;
 	//unsigned char input;
 	//input = engine_input_manager_hold( input_type_right );
 	//if( input )
@@ -35,6 +53,6 @@ void screen_func_screen_update( unsigned char *screen_type )
 	//}
 
 	//engine_sprite_manager_draw( 40, 40, SPRITE_TILES + 24 );
-	engine_cursor_manager_draw( 24, 24, CURSOR_TILES );
+	engine_cursor_manager_draw( sx, sy, CURSOR_TILES );
 	*screen_type = screen_type_func;
 }
--- dev/screen/title_screen.c
+++ dev/screen/title_screen.c
@@ -18,7 +18,7 @@ void screen_title_screen_load()
 	engine_content_manager_load_sprites();
 	engine_content_manager_load_title();
 	engine_font_manager_draw_text( LOCALE_PRESS_START, 10, 21 );
-	
+
 	engine_font_manager_draw_text( LOCALE_RECORD_TEXT, 6, 12 );
 	engine_font_manager_draw_text( LOCALE_COVERS_TEXT, 20, 12 );
 	devkit_SMS_displayOn();
